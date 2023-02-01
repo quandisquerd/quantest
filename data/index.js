80e8f26beb8c287c921d6ca9292ff032964d5a10
quan1234 <quanvdph19781@fpt.edu.vn>
2023-02-01 15:43:00 +0700

    bai7

---
+++ data/index.js
@@ -0,0 +1,10 @@
+const menu = [
+    {name: "Home", path: "/"},
+    { name: "About", path: "/about" },
+    { name: "Content", path: "/content" },
+    { name: "Project", path: "/project" },
+    { name: "Admin", path: "/admin/project" },
+  
+]
+
+export {menu}
