714335fc4f0f3418a4679407004825fef32e16a0
quan1234 <quanvdph19781@fpt.edu.vn>
2023-02-01 21:36:40 +0700

    ok

--- page/admin/product.js
+++ page/admin/product.js
@@ -5,18 +5,42 @@ import { useEffect, useState } from "../../lib"
 
 const product = () => {
     const [data,setData]= useState(List);
+    
+    // setData(dataList);
+    useEffect(function () {
+      
+        const btnn = document.querySelectorAll('.btn-new');
+        console.log(btnn);
+        for (let btn of btnn) {
+            const id = btn.dataset.id;
+             
+            btn.addEventListener('click', function () {
+               const idd= List.length+1
+                const newData = (previousData) => [...previousData, {
+                    id: idd,
+                    name: "san pham 3",
+                    img: "https://cpad.ask.fm/450/774/576/-29996968-1tfd7tc-gpggmmc5d0og3a0/original/image.jpg"
+                }];
+                console.log(idd)
+                setData(newData)
+            })
+        }
+    })
     useEffect(function(){
         const btns= document.querySelectorAll('.btn-remove');
-        console.log(btns);
+  
+      
         for (let btn of btns) {
             const id = btn.dataset.id;
             btn.addEventListener('click',function(){
                 const newData = data.filter((project)=>project.id!=id);
+                
                 setData(newData);
             })
         }
     })
   return `${header()}
+  <button class="btn-new">New</button>
   <table border="1px" cellspacing="1px" cellpadding="1px">
       <thead>
         <th>Stt</th>
